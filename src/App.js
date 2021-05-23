7df2e529b4872ec78adb611ac634c232d8f1bbd7
muhamdfikrii <fikri5741@gmail.com>
2021-05-23 22:31:43 +0700

    Adding Sidebar Components to App

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./parts/Header";
+import Sidebar from "./parts/Sidebar";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <>
-          <Switch>
-            <Route path="/">
-              <Header />
-            </Route>
-          </Switch>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Switch>
+              <Route path="/">{/* Chat */}</Route>
+            </Switch>
+          </AppBody>
         </>
       </Router>
     </div>
@@ -20,3 +24,8 @@ function App() {
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
