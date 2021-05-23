ffb2c9148727b1ce0b7f25217fc13a66e15aa11f
muhamdfikrii <fikri5741@gmail.com>
2021-05-23 20:06:29 +0700

    Adding & Finishing Header Component

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,7 +1,91 @@
+import { Avatar } from "@material-ui/core";
+import { AccessTime, HelpOutline, Search } from "@material-ui/icons";
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
-  return <div></div>;
+  return (
+    <HeaderContainer>
+      {/* Header Left */}
+      <HeaderLeft>
+        <HeaderAvatar //TODO: Add OnClick
+        />
+        <AccessTime />
+      </HeaderLeft>
+      {/* Header Search */}
+      <HeaderSearch>
+        <Search />
+        <input type="text" placeholder="Search Something" />
+      </HeaderSearch>
+      {/* Header Right */}
+      <HeaderRight>
+        <HelpOutline />
+      </HeaderRight>
+    </HeaderContainer>
+  );
 }
 
 export default Header;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  background-color: var(--slack-color);
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  color: white;
+`;
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  display: flex;
+  align-items: center;
+  opacity: 1;
+  border: none;
+  background-color: #421f44;
+  flex: 0.4;
+  border-radius: 6px;
+  padding: 0 50px;
+  border: solid 1px gray;
+  color: gray;
+  text-align: center;
+  > input {
+    background: transparent;
+    border: none;
+    text-align: center;
+    color: white;
+    min-width: 30vw;
+    :focus {
+      outline: none;
+    }
+  }
+`;
+
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  justify-content: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-right: 20px;
+  }
+`;
